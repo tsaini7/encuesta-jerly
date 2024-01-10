8e7ac11a1bcf13ceb47e20a2a0cd57a21446e2de
Tomas Saini <107080126+tsaini7@users.noreply.github.com>
2024-01-09 21:23:08 -0300

    Subida de encuesta de Jerly

---
+++ enviar_encuesta.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+if( $SERVER [ 'REQUEST_METHOD'] != 'POST'){
+    header('Location: index.html');
+}
+
+//Variables
+$empresa = $_POST['empresa'];
+$solucion = $_POST['solucion-de-problemas'];
+$presentacion = $_POST['presentacion-del-personal'];
+$desempeño = $_POST['desempeño-del-personal'] ;
+$calidad = $_POST['calidad-de-servicio'];
+$rapidez =  $_POST['rapidez-de-respuesta'];
+$referencia = $_POST['referencia'];
+
+$para = 'soporte@jerlysecurity.com.ar'; 
+$subject = "Resultado de la encuesta de $empresa";
+
+// Mensaje que se envia al mail
+
+$mensaje = <<< HTML
+    <div style= "padding: 1rem; border: 1px solid">
+    <h2 style="text-align: center"> Encuesta de satisfacción de <strong>$empresa</strong> </h2>
+    <p style="text-align: center"><strong>Solución de Problemas:</strong> $solucion </p>
+    <p style="text-align: center"><strong>Presentación del Personal:</strong> $presentacion </p>
+    <p style="text-align: center"><strong>Desempeño del Personal:</strong> $desempeño </p>
+    <p style="text-align: center"><strong>Calidad de Servicios:</strong> $calidad </p>
+    <p style="text-align: center"><strong>Rapidez en la Respuesta:</strong> $rapidez </p>
+    <p style="text-align: center"><strong>Referencia:</strong> $referencia </p>
+    <p style="text-align: center"> Encuesta creada por Tomas Saini (tomas.saini@msn.com)</p>
+    </div>
+    HTML;
+
+$headers = "MIME-Version: 1.0 \r\n";
+$headers.= "Content-type: text/html; charset=utf-8 \r\n";
+$headers.= "From: Enviado desde la encuesta online \r\n";
+
+
+
+if (mail($para , "Encuesta de satisfacción de $empresa", $mensaje, $headers)) {
+    header ("Location: gracias.html");
+    exit();
+} else {
+    echo ("Hubo un error y no se pudo enviar la encuesta, intente nuevamente");
+}
+
+
+
+?>
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+Esta encuesta fue desarrollada para la empresa Jerly Security. 
+Se utilizó: HTML, CSS y PHP
+El ultimo fue utilizado para el envio del formulario que llega a traves de Email.
+
+Desarrollo hecho por Tomas Saini
